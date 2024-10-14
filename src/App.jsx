7984c23037c2f8005d22fd841c1c2e56125fce34
Georgefdz <georgfdz@gmail.com>
2024-10-14 13:04:05 -0600

    V0.7 Feat: Removed files and started cleaning for release

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./Home.jsx";
-import Books from "./Books.jsx";
-import Podcasts from "./Podcasts.jsx";
 import Mobile from "./Mobile.jsx";
 import "./App.css";
 import PodcastsMobile from "./PodcastsMobile.jsx";
@@ -12,10 +9,6 @@ const App = () => {
     <>
       <Router>
         <Routes>
-          {/* <Route path='/' element={<Home />} /> */}
-          <Route path='/books' element={<Books />} />
-          <Route path='/podcasts' element={<Podcasts />} />
-          {/* <Route path='/mobile' element={<Mobile />} /> */}
           <Route path='/' element={<Mobile />} />
           <Route path='/podcastsmobile' element={<PodcastsMobile />} />
           <Route path='/booksmobile' element={<BooksMobile />} />
